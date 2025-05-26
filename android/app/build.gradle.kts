2632668ff7559fb4d01569d0fc85f64b55d83ef2
rzptr30 <mrezaputrap@gmail.com>
2025-05-27 04:25:34 +0700

    balik gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,60 +1,44 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-// Baca local.properties
-val localPropertiesFile = rootProject.file("local.properties")
-val localProperties = java.util.Properties()
-if (localPropertiesFile.exists()) {
-    localProperties.load(localPropertiesFile.inputStream())
-}
-
-// Konfigurasi Flutter
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toIntOrNull() ?: 1
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
-
 android {
-    namespace = "com.example.bantoo_fix" // Ubah sesuai dengan package name aplikasi Anda
-    compileSdk = 33
-    
-    ndkVersion = null // PENTING: Hapus ketergantungan pada NDK
-    
-    defaultConfig {
-        applicationId = "com.example.bantoo_fix" // Ubah sesuai dengan package name aplikasi Anda
-        minSdk = 21
-        targetSdk = 33
-        versionCode = flutterVersionCode
-        versionName = flutterVersionName
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
+    namespace = "com.example.pemmob"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.pemmob"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
 }
 
 flutter {
-    source = ".."
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.10")
+    source = "../.."
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -23,17 +11,9 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-    
-    // PENTING: Tambahkan konfigurasi ini untuk mengatasi masalah NDK
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            project.extensions.findByName("android")?.apply {
-                // Sesuaikan dengan versi NDK yang tersedia di sistem Anda
-                // ATAU hapus baris ini jika tidak butuh NDK
-                (this as com.android.build.gradle.BaseExtension).ndkVersion = null
-            }
-        }
-    }
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
